7de4ef7c7d01c2dd4ef47a1197ce2660ab0cb32c
rdj20 <rafael.sandovalg4@gmail.com>
2022-09-10 02:21:09 -0600

    Prueba 5

---
+++ test/helper/helperdata.js
@@ -0,0 +1,22 @@
+const testDataTrack = {
+    name: "Ejemplo",
+    album: "Ejemplo",
+    cover: "http://image.com",
+    artist: {
+      name: "Ejemplo",
+      nickname: "Ejemplo",
+      nationality: "VE",
+    },
+    duration: {
+      start: 1,
+      end: 3,
+    },
+    mediaId: "",
+  };
+  module.exports = {
+    testAuthRegister,
+testAuthLogin,
+testAuthRegisterAdmin,
+testStorageRegister,
+testDataTrack
+}
